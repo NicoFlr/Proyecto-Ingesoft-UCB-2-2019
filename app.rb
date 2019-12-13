82356b4b561f3b9b9f906e1cca1414578e4b0d31
Randoomed <nico_fr1110@hotmail.com>
2019-12-12 23:52:25 -0400

    error fixed in app.rb

--- app.rb
+++ app.rb
@@ -20,35 +20,35 @@ get "/setup-arena" do
 end
 
 post "/addVehicle" do
-begin
-  game.set_up_instruction params[:instructions], params[:posX].to_i, params[:posY].to_i, params[:orientation]
-  erb :vehiclesetup
-rescue InvalidXPosition
-  erb :posicioninvalidax
-rescue InvalidYPosition
-  erb :posicioninvaliday
-rescue InvalidOrientation
-  erb :orientacionInvalida
+  begin
+    game.set_up_instruction params[:instructions], params[:posX].to_i, params[:posY].to_i, params[:orientation]
+    erb :vehiclesetup
+  rescue InvalidXPosition
+    erb :posicioninvalidax
+  rescue InvalidYPosition
+    erb :posicioninvaliday
+  rescue InvalidOrientation
+    erb :orientacionInvalida
+  end
 end
 
 post "/play" do
-  @field = Field.new
-
-  @initial_instructions = game.get_instruction
-  begin
-    @field.set_rows params[:fieldRows].to_i
-    @field.set_columns params[:fieldColumns].to_i
-    game.set_field @field
-    game.execute_instructions
-    @instructions_after = game.get_instruction
-    erb :game
-  rescue InvalidXMeasures
-    erb :medidasinvalidasfilas
-  rescue InvalidYMeasures
-    erb :medidasinvalidascolumnas
-  rescue InvalidInstruction
-    erb :instruccioninvalida
-  rescue OutOfFieldError
-    erb :fueradeterreno
-  end
+    @field = Field.new
+    @initial_instructions = game.get_instruction
+    begin
+      @field.set_rows params[:fieldRows].to_i
+      @field.set_columns params[:fieldColumns].to_i
+      game.set_field @field
+      game.execute_instructions
+      @instructions_after = game.get_instruction
+      erb :game
+    rescue InvalidXMeasures
+      erb :medidasinvalidasfilas
+    rescue InvalidYMeasures
+      erb :medidasinvalidascolumnas
+    rescue InvalidInstruction
+      erb :instruccioninvalida
+    rescue OutOfFieldError
+      erb :fueradeterreno
+    end
 end
